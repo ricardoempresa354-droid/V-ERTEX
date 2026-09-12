4a0943cced86e58d3426ce5b6f261fa2143502b2
ricardoempresa354-droid <ricardoempresa354@gmail.com>
2026-09-12 10:58:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -727,15 +727,7 @@ app.get(
     res.sendFile(
       path.join(
         __dirname,
-        "public",
-        "index.html"
-      )
-    )
-);
-
-app.listen(
-  PORT,
- app.get("/{*splat}", (req, res) => {
+       app.get("/{*splat}", (req, res) => {
   res.sendFile(
     path.join(__dirname, "public", "index.html")
   );
